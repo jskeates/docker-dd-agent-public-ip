f220adbe04173cb64dd803db0c10b6a998047a46
Jake Skeates <jskeates@me.com>
2017-03-31 09:27:13 +0000

    prevent dd-agent from detecting its on k8s

--- wrapper.sh
+++ wrapper.sh
@@ -5,4 +5,6 @@ EXTERNAL_IP=$(curl ifconfig.me/ip)
 echo "Public IP of this node is ${EXTERNAL_IP}"
 
 echo "Starting dd-agent..."
-KUBERNETES_KUBELET_HOST=${EXTERNAL_IP} /entrypoint.sh supervisord -n -c /etc/dd-agent/supervisor.conf
+export KUBERNETES_KUBELET_HOST=${EXTERNAL_IP} 
+unset KUBERNETES_PORT
+/entrypoint.sh supervisord -n -c /etc/dd-agent/supervisor.conf
